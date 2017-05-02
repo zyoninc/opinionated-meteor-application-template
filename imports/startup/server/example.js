c51f2b269d5027447c8386f2d40ce79c27936f0e
Sean Dennison <sean.dennison@twintechs.com>
2017-05-02 07:58:42 -0400

    ensure default userId is added to seeded Examples

--- imports/startup/server/example.js
+++ imports/startup/server/example.js
@@ -1,10 +1,11 @@
 import { ExampleCollection } from 'meteor/justinr1234:example';
-
-const seeds = [
-  { name: 'Basket', quantity: 3 },
-  { name: 'Bicycle', quantity: 2 },
-];
+import { Meteor } from 'meteor/meteor';
 
 if (ExampleCollection.find().count() === 0) {
+  const userId = Meteor.users.findOne({ username: Meteor.settings.defaultAccount.username })._id;
+  const seeds = [
+    { name: 'Basket', quantity: 3, userId },
+    { name: 'Bicycle', quantity: 2, userId },
+  ];
   seeds.forEach(e => ExampleCollection.insert(e));
 }
